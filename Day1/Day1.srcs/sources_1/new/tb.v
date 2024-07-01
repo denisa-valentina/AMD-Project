7917a2d49e99c3e5dbd49330d714c2aa1fb539b9
denisa-valentina <160639346+denisa-valentina@users.noreply.github.com>
2024-07-01 12:45:00 +0300

    Add files via upload
    Primul proiect in cadrul AMD-ului. Bafta

---
+++ Day1/Day1.srcs/sources_1/new/tb.v
@@ -0,0 +1,49 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: AMD 
+// Engineer: Aeloaiei Denisa-Valentina
+// 
+// Create Date: 01.07.2024 11:36:30
+// Design Name: 
+// Module Name: top
+// Project Name: First Vivado Project
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+
+module AND_1(a, b, o);
+    input a, b;
+    output [1:0] o;
+    assign o[1] = a & b;
+    assign o[0] = a ^ b;
+    
+endmodule
+
+module test;
+    reg a, b;
+    wire [1:0]o;
+    
+    AND_1 and_inst(a, b, o);
+    
+    initial
+        begin
+            #0 a = 1'b0; b = 1'b0;
+            #10 a = 1'b1;
+            #10 b = 1'b1;
+            #10 b = 1'b0; a = 1'b0;
+        
+        end
+
+    initial
+        #100 $finish;
+endmodule
