8002df49772c74d9cfa6b072dc831c936476edd6
denisa-valentina <160639346+denisa-valentina@users.noreply.github.com>
2024-07-03 22:38:09 +0300

    Add files via upload
    Implementarea a 4 bistabile folosind PIPO și SISO

---
+++ Tema 3/sisoTop.v
@@ -0,0 +1,11 @@
+`timescale 1ns / 1ps
+module sisoTop(clk, reset, pl, di, do);
+    input clk, reset, pl;
+    input di;
+    output [3:0] do;
+    
+    bistabil inst3(clk, reset, pl, di, do[3]);
+    bistabil inst2(clk, reset, pl, do[3], do[2]);
+    bistabil inst1(clk, reset, pl, do[2], do[1]);
+    bistabil inst0(clk, reset, pl, do[1], do[0]);
+endmodule
