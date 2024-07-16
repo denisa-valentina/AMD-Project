54700c3d7daf906b4b1ee0fadea3e94a9648b0c3
denisa-valentina <160639346+denisa-valentina@users.noreply.github.com>
2024-07-16 13:58:54 +0300

    Update top.v

--- MIPS/MIPS-v6/MIPS-v5/MIPS-v5.srcs/sources_1/new/top.v
+++ MIPS/MIPS-v6/MIPS-v5/MIPS-v5.srcs/sources_1/new/top.v
@@ -2,15 +2,16 @@
 
 module top (input clk);
         
-        wire [31:0] pc, pc_in;
+        wire [31:0] pc, pc_in, branch, pc_add, f;
         wire [31:0] instruction;
         wire [3:0] ALU_OP;
-        wire ZERO, REGWRITE, REGDST, EXTOP, ALUSRC, MEMWRITE, MEM2REG;
+        wire ZERO, REGWRITE, REGDST, EXTOP, ALUSRC, MEMWRITE, MEM2REG, PCSRC;
         
         wire [31:0] write_data;
         wire [4:0] write_address;
         wire [31:0] RD1, RD2;
         wire [31:0] extend_sign;
+        wire [31:0] shift_out;
         wire [31:0] aluSrcB;
         wire [31:0] alu_Out;
         wire [31:0] RD;
@@ -47,7 +48,16 @@ module top (input clk);
         //MEM2REG
         mux2_1 #(32)mem2reg_mux(RD, alu_Out, MEM2REG, write_data); 
         
-        //reg
+        
+        //verifica de aici :(
+        //SHIFT LEFT 2
+        //shift_reg shift_left2(extend_sign, shift_out);
+        
+        //PC ADDER (FOR BEQ)
+        //adder adder_inst(pc_in, shift_out, branch);
+        
+        //PCSource mux
+        //mux2_1 #(32)muxPcSource_inst(pc_in, branch, PCSRC, f); // vezi aici
              
     
 endmodule
